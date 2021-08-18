efce5b2889a70f0b33b70f9c88a72ab2b542aadf
LucasAlves011 <lucas.nascimento.al16@gmail.com>
2021-08-18 20:37:05 -0300

    adicionado método de post para categoria

--- src/main/java/com/principal/resources/CategoriaResources.java
+++ src/main/java/com/principal/resources/CategoriaResources.java
@@ -5,11 +5,10 @@ import com.principal.dtos.CategoriaDTO;
 import com.principal.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,5 +31,19 @@ public class CategoriaResources {
         List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = categoriaService.create(obj);
+        /*
+          Por boas práticas de programação é recomendado sempre que criado alguma classe no back-end, retornar
+          uma URI de acesso a esse novo objeto ao client.
+         */
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        /* Entendi oque esse método faz, mas não entendi absolutamente nada de como construir esse método.
+         Sla Java às vezes têm umas coisas que são bem esquisitas */
+        return ResponseEntity.created(uri).build();
+    }
+
 
 }
